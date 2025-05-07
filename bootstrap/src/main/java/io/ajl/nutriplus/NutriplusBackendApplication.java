727755d01c2030e660cf52a09b0a1381f38c79c9
Antoine Pourpoint <antoine.pourpoint@gmail.com>
2025-05-07 23:19:25 +0200

    inital commit

---
+++ bootstrap/src/main/java/io/ajl/nutriplus/NutriplusBackendApplication.java
@@ -0,0 +1,13 @@
+package io.ajl.nutriplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriplusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutriplusBackendApplication.class, args);
+    }
+
+}
